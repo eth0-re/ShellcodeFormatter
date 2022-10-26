c42298ed4a67b5deffe077a48cdeeab6f406712b
eth0 <e@eth0.re>
2022-10-26 11:34:20 +0100

    Remove asciihex dependency Process raw bytes instead of hex string Fix bug in csharp definition

--- transforms/definitions.py
+++ transforms/definitions.py
@@ -1,7 +1,7 @@
 
 
 class ShellcodeDefinition:
-    def __init__(self, name, output_format, byte_format, byte_separator, bytes_per_line:int, line_format, first_line_format=None):
+    def __init__(self, name, output_format, byte_format, byte_separator, bytes_per_line:int, line_format, first_line_format=None, last_line_format=None):
         self.name = name
         self.output_format = output_format
         self.byte_format = byte_format
@@ -9,16 +9,17 @@ class ShellcodeDefinition:
         self.bytes_per_line = bytes_per_line
         self.line_format = line_format
         self.first_line_format = first_line_format
-
+        self.last_line_format = last_line_format
 
 definitions = {
     "csharp": ShellcodeDefinition(
         name="csharp",
-        output_format="byte[] buf = new byte[ ~~BYTE_COUNT~~ ] { ~~LINES~~ }",
+        output_format="byte[] buf = new byte[ ~~BYTE_COUNT~~ ] { \n~~LINES~~};",
         byte_format="0xff",
         byte_separator=",",
         bytes_per_line=16,
-        line_format="~~BYTES~~\n"
+        line_format="~~BYTES~~,\n",
+        last_line_format="~~BYTES~~\n"
     ),
     "powershell": ShellcodeDefinition(
         name="powershell",
--- transforms/transformer.py
+++ transforms/transformer.py
@@ -1,5 +1,4 @@
 from io import BytesIO
-import binascii
 
 from transforms.definitions import ShellcodeDefinition
 
@@ -7,7 +6,7 @@ def _split_lines(hex_bytes, definition:ShellcodeDefinition):
     if definition.bytes_per_line == 0:
         line_length = len(hex_bytes)
     else:
-        line_length = definition.bytes_per_line * 2
+        line_length = definition.bytes_per_line
     
     raw_lines:list = [hex_bytes[i:i+line_length] for i in range(0, len(hex_bytes), line_length)]
 
@@ -47,15 +46,79 @@ def _format_lines(raw_lines:list, definition:ShellcodeDefinition):
     lines = "".join(lines)
     return lines
 
+def _format_lines_ng(raw_lines:list, definition:ShellcodeDefinition):
+    lines = list();
+
+    for line_number, raw_line in enumerate(raw_lines):
+        # raw_line = b'\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\xff'
+
+        if "ff" in definition.byte_format:
+            # Lowercase hex format
+
+            bytes = []
+            for raw_byte in raw_line:
+                # raw_byte = b'\x01'
+                byte = definition.byte_format.replace("ff", f"{raw_byte:02x}")
+                bytes.append(byte)
+                
+            line = definition.byte_separator.join(bytes)
+
+
+        elif "FF" in definition.byte_format:
+            # Uppercase hex format
+            bytes = []
+            for raw_byte in raw_line:
+                # raw_byte = b'\x01'
+                byte = definition.byte_format.replace("FF", f"{raw_byte:02X}")
+                bytes.append(byte)
+                
+            line = definition.byte_separator.join(bytes)
+
+        elif "255" in definition.byte_format:
+            # Decimal format
+
+            bytes = []
+            for raw_byte in raw_line:
+                # raw_byte = b'\x01'
+                byte = definition.byte_format.replace("255", f"{raw_byte:03d}")
+                bytes.append(byte)
+                
+            line = definition.byte_separator.join(bytes)
+
+        elif "377" in definition.byte_format:
+            # Octal format
+
+            bytes = []
+            for raw_byte in raw_line:
+                # raw_byte = b'\x01'
+                byte = definition.byte_format.replace("377", f"{raw_byte:03o}")
+                bytes.append(byte)
+                
+            line = definition.byte_separator.join(bytes)
+
+        else:
+            raise Exception("Invalid byte format")
+
+
+        if line_number == 0 and definition.first_line_format is not None:
+            # First line
+            lines.append(definition.first_line_format.replace("~~BYTES~~", line))
+        elif line_number == len(raw_lines) - 1 and definition.last_line_format is not None:
+            # Last line
+            lines.append(definition.last_line_format.replace("~~BYTES~~", line))
+        else:
+            # All other lines
+            lines.append(definition.line_format.replace("~~BYTES~~", line))
+
+    # Combne the lines into a single string
+    lines = "".join(lines)
+    return lines
 
 
 def transform_shellcode(shellcode:BytesIO, definition:ShellcodeDefinition):
-    # First transform the raw bytes into hex
-    hex_bytes = binascii.hexlify(shellcode)
+    raw_lines = _split_lines(shellcode, definition)
 
-    raw_lines = _split_lines(hex_bytes, definition)
-
-    lines = _format_lines(raw_lines, definition)
+    lines = _format_lines_ng(raw_lines, definition)
     
     # Replace the output format with the lines
     output = definition.output_format.replace("~~LINES~~", lines)
