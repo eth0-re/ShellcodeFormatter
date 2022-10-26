132fb2d72709f93f66a757a2ce40ec3a973aac89
eth0 <e@eth0.re>
2022-10-26 11:35:13 +0100

    Remove old line formatter code

--- transforms/transformer.py
+++ transforms/transformer.py
@@ -16,40 +16,6 @@ def _format_lines(raw_lines:list, definition:ShellcodeDefinition):
     lines = list();
 
     for line_number, raw_line in enumerate(raw_lines):
-        # Convert the hex bytes into the byte format
-        if definition.byte_format == "0xff":
-            # Convert the hex bytes into 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f
-            bytes = [f"0x{raw_line[i:i+2].decode('ascii')}" for i in range(0, len(raw_line), 2)]
-            # Join the bytes with the byte separator
-            line = definition.byte_separator.join(bytes)
-
-        elif definition.byte_format == "\\xff":
-            # Convert the hex bytes into \x0a, \x0b, \x0c, \x0d, \x0e, \x0f
-            bytes = [f"\\x{raw_line[i:i+2].decode('ascii')}" for i in range(0, len(raw_line), 2)]
-            # Join the bytes with the byte separator
-            line = definition.byte_separator.join(bytes)
-
-        elif definition.byte_format == "255":
-            # Convert the hex bytes into dec 255 with leading zeros
-            bytes = [f"{int(raw_line[i:i+2].decode('ascii'), 16):03}" for i in range(0, len(raw_line), 2)]
-            
-            # Join the bytes with the byte separator
-            line = definition.byte_separator.join(bytes)
-
-        # Replace the line format with the line
-        if line_number == 0 and definition.first_line_format is not None:
-            lines.append(definition.first_line_format.replace("~~BYTES~~", line))
-        else:
-            lines.append(definition.line_format.replace("~~BYTES~~", line))
-
-    # Combne the lines into a single string
-    lines = "".join(lines)
-    return lines
-
-def _format_lines_ng(raw_lines:list, definition:ShellcodeDefinition):
-    lines = list();
-
-    for line_number, raw_line in enumerate(raw_lines):
         # raw_line = b'\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\xff'
 
         if "ff" in definition.byte_format:
@@ -118,7 +84,7 @@ def _format_lines_ng(raw_lines:list, definition:ShellcodeDefinition):
 def transform_shellcode(shellcode:BytesIO, definition:ShellcodeDefinition):
     raw_lines = _split_lines(shellcode, definition)
 
-    lines = _format_lines_ng(raw_lines, definition)
+    lines = _format_lines(raw_lines, definition)
     
     # Replace the output format with the lines
     output = definition.output_format.replace("~~LINES~~", lines)
