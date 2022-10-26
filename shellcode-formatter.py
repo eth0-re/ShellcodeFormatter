6f275abbe38491a116813dae16290e1c0b836cd8
eth0 <e@eth0.re>
2022-10-26 14:41:44 +0100

    Add examples and details in README
    Improve comments throughout

--- shellcode-formatter.py
+++ shellcode-formatter.py
@@ -10,7 +10,7 @@ import transforms.definitions as definitions
 
 
 def main(args):
-    
+
     raw_shellcode:BytesIO = args.infile.read()
     definition = definitions.definitions[args.format]
     output = transformer.transform_shellcode(raw_shellcode, definition)
@@ -21,7 +21,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('infile', nargs='?', type=argparse.FileType('rb'), default=sys.stdin.buffer)
-    
+
     parser.add_argument('-f', help='Output format', choices=definitions.definitions.keys(), required=True, dest='format')
     parser.add_argument('-o', '--outfile', help='Output file')
 
